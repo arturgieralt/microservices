4dabcca2122b196cbacb758874bfe0665640ea94
ArturGieralt <artur.gieralt@vonage.com>
2019-10-30 15:34:26 +0100

    Added tests for Parent Service

---
+++ parentService/src/components/IFrame.test.jsx
@@ -0,0 +1,54 @@
+import React from 'react';
+import IFrame from './Iframe';
+import Enzyme from 'enzyme';
+import Adapter from 'enzyme-adapter-react-16';
+import { shallow, mount } from 'enzyme'
+
+Enzyme.configure({ adapter: new Adapter() });
+
+describe('IFrame component', () => {
+
+    it('should register message listener on mount', () => {
+        const map = {}
+        window.addEventListener = jest.fn((event, cb) => {
+            map[event] = cb;
+        })
+
+        const component = shallow(<IFrame name='name' url='url' />)
+        expect(map).toHaveProperty('message', expect.any(Function))
+    })
+
+    it('should unregister message listener on unmount', () => {
+        
+        window.removeEventListener = jest.fn()
+        const component = shallow(<IFrame name='name' url='url' />)
+
+        component.unmount()
+
+        expect(window.removeEventListener).toHaveBeenCalledWith('message', expect.any(Function))
+    })
+
+    it('should post initial data when iFrame is loaded', () => {
+        
+        const component = shallow(<IFrame name='name' url='url' />)
+        const instance =  component.instance()
+
+        const postMessage = jest.fn()
+
+        instance.iframeRef.current = {
+          contentWindow: {
+            postMessage,
+            addEventListener: jest.fn()
+          }
+        }
+
+        component.find('iframe').prop('onLoad')()
+
+        expect(postMessage).toBeCalledTimes(1)
+        expect(postMessage).toHaveBeenCalledWith({
+            "payload": {"data": "Data from parent for service: name"}, 
+            "type": "INIT_UPDATE"}, 
+            "*"
+        )
+    })
+})
